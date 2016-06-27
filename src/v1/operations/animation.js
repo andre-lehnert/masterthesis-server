4d6de57e30ad77f8541b692e899c33bf3dfd471a
andre-lehnert <contact@andre-lehnert.de>
2016-06-27 20:38:13 +0000

    Animation

--- src/v1/operations/animation.js
+++ src/v1/operations/animation.js
@@ -1,27 +1,62 @@
 // ---------------- Operation --------------------------------------------------
 
-var express = require('express'),
+var bodyParser = require('body-parser'),
+    express = require('express'),
     // Server
     server = require('./../../../webserver'),
     URL = server.serverUrl,
     // REST-API-Router
     api = require('./../rest-api'),
     // Database Access (salesforce REST-API)
-    db = require('./../database/salesforce/database');
+    db = require('./../database/salesforce/database'),
+
+    i2c = require('masterthesis-i2c-library');
     // Express.js Application
     app = module.exports = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 // ---------------- API --------------------------------------------------------
 
 var OPERATION = '/animation';
 
-// ---------------- Routing ----------------------------------------------------
+
+// ---------------- Operations --------------------------------------------
+
+var requestBars = function (req, res, next) {
+  db.getBars(req, res, next);
+};
+
+var requestBar = function (req, res, next) {
+  db.getBar(req, res, next);
+};
+
+var requestAnimation = function (req, res, next) {
+  db.getAnimation(req, res, next);
+};
+
+
+
+// ---------------- Routing -----------------------------------------------
 
 /*
  * ## List all receivers
  */
-app.get('/', function(req, res) {
-
-  db.getBars(req, res);
-
+app.get('/', [requestBars], function(req, res) {
+  res.json(req.response);
 });
+
+/*
+ * ## Get a bar by label
+ */
+app.get('/:label', [requestBar], function(req, res) {
+ res.json(req.response);
+});
+
+/*
+ * ## Get a bar by label
+ */
+app.get('/:label/:animation', [requestBar, requestAnimation], function(req, res) {
+ res.json(req.response);
+});
+
